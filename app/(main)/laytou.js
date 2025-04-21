785659d346d056675dd73655741caf028bc3c34b
kundalik jadhav <kundalikjadhav0807@gmail.com>
2025-04-21 10:49:33 +0530

    chore: update dependencies and add new SVG logo
    - Added several Radix UI components and utility libraries to dependencies.
    - Updated existing dependencies for better performance and features.
    - Introduced a new SVG logo file for branding purposes.

---
+++ app/(main)/laytou.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MainLayout = ({ children }) => {
+  return <div className="max-w-5xl py-5">{children}</div>;
+};
+
+export default MainLayout;
---
+++ app/(main)/to-do-list/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TodoListPage = () => {
+  return <div>To do List Page</div>;
+};
+
+export default TodoListPage;
